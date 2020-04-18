d4f7c593a5d4e9d6c5cad4cea5592a4025f19ddb
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-18 09:03:27 -0300

    Using inquirer

--- backend/app.js
+++ backend/app.js
@@ -54,12 +54,16 @@ const args = require('minimist')(process.argv.slice(2,3));
 //   }
 // }, 100)
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-  
-readline.question(`What's your name?`, name => {
-    console.log(`Hi ${name}!`)
-    readline.close()
+const inquirer = require('inquirer')
+
+var questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?"
+  }
+]
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers['name']}!`)
 })
