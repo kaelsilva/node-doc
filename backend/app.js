fe1624834362aeec0616d55420947f1dc75d477f
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-04-18 15:48:37 -0300

    Using import and export from different files

--- backend/app.js
+++ backend/app.js
@@ -1,69 +1,9 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(process.env.LOGNAME));
+const car = require('./car');
 
-const server = app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => res.send(`${car.brand} ${car.model}`));
 
-const args = require('minimist')(process.argv.slice(2,3));
-
-// args.forEach((val, index)=>console.log(`${index}: ${val}`));
-// console.log(args['name']);
-
-// process.argv.forEach((val, index) => {
-//   console.log(`${index}: ${val}`)
-// })
-
-// console.log('%o', Number);
-
-// const doSomething = () => console.log('test')
-// const measureDoingSomething = () => {
-//   console.time('doSomething()')
-//   //do something, and measure the time it takes
-//   doSomething()
-//   console.timeEnd('doSomething()')
-// }
-// measureDoingSomething()
-
-// const chalk = require('chalk')
-// console.log(chalk.yellowBright.bgGreen('Brazil'))
-
-// const ProgressBar = require('progress')
-
-// const bar = new ProgressBar(':bar', { total: 100 })
-// const timer = setInterval(() => {
-//   bar.tick()
-//   if (bar.complete) {
-//     clearInterval(timer)
-//   }
-// }, 100)
-
-const inquirer = require('inquirer')
-
-var questions = [
-  {
-    type: 'input',
-    name: 'name',
-    message: "What's your name?"
-  }
-]
-
-inquirer.prompt(questions).then(answers => {
-  console.log(`Hi ${answers['name']}!`)
-})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
